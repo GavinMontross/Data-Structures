8b45496c7fc1a2a06e9d466e6437b6df29073131
GSM429 <gavmontross@gmail.com>
2024-08-27 12:12:32 -0400

    day1

---
+++ Practice/src/Day_one.java
@@ -0,0 +1,20 @@
+// Data Structures 
+// August 27, 2024
+// Max time is O(n^2)
+import java.util.Arrays;
+public class Day_one {
+    public static int[] twoSum(int[] nums, int target){
+        for(int i = 0; i < nums.length; i ++){
+            for(int j = i+1; j < nums.length; j ++){
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[0];
+    }
+    public static void main(String[] args) {
+        int[] checks = {1,2,3,4,5,6,7};
+        System.out.println("The two indicies that add up to 12 are: " + Arrays.toString(twoSum(checks, 12)));
+    }
+}
