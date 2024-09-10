f0ca4cb7b39d904a7aa51fa9abc3b7e5dd11cfd0
GSM429 <gavmontross@gmail.com>
2024-09-10 13:23:39 -0400

    Finished up SinglyLinkedList

--- Classwork/src/LinkedLists/MainClassDemo.java
+++ Classwork/src/LinkedLists/MainClassDemo.java
@@ -8,10 +8,33 @@ public class MainClassDemo {
         SLL.insertAtEnd(10);
         SLL.insertAtEnd(20);
         SLL.insertAtEnd(30);
+        System.out.print("Initial List: ");
         SLL.display();
+        System.out.println();
 
         SLL.insertAfter(20, 25);
+        System.out.print("Inserting 25 after 20: ");
         SLL.display();
+        System.out.println();
+        System.out.print("Current total Nodes: ");
         System.out.println(SLL.totalNodes());
+        System.out.println();
+
+        SLL.insertAtFront(5);
+        System.out.print("Inserting 5 at the front: ");
+        SLL.display();
+        System.out.println();
+
+        SLL.insertBefore(20, 15);
+        System.out.print("Inserting 15 before 20: " );
+        SLL.display();
+        System.out.println();
+        SLL.delete(30);
+        System.out.print("Deleting 30: ");
+        SLL.display();
+        System.out.println();
+        SLL.deleteAfter(10);
+        System.out.print("Deleting after Node with data 10: ");
+        SLL.display();
     }
 }
--- Classwork/src/LinkedLists/SinglyLinkedList.java
+++ Classwork/src/LinkedLists/SinglyLinkedList.java
@@ -60,4 +60,65 @@ public void insertAfter(int afterData, int newData){
         System.out.println("Node with data " + afterData + " not found.");
     }
 }
+
+public void insertAtFront(int data) {
+    Node newNode = new Node(data);
+    if (head == null){
+        newNode = head;
+    }
+    else{
+        newNode.next = head;
+        head = newNode;
+
+    }}
+    
+public void insertBefore(int beforeData, int newData) {
+    Node current = head;
+    while(current != null && current.next != null && current.next.data != beforeData){
+        current = current.next;
+    }
+    if (current != null && current.next != null){
+        Node newNode = new Node(newData);
+        newNode.next = current.next;
+        current.next = newNode;
+    }
+    else{
+        System.out.println("Node with data " + beforeData + " could not be found.");
+    }
 }
+
+public void delete(int data){
+    if (head == null){
+        System.out.println("List is empty.");
+        return;
+    }
+    if (head.data == data){
+        head = head.next;
+        return;
+    }
+    Node current = head;
+    while(current != null && current.next.data != data){
+        current = current.next;}
+        if(current.next != null){
+            current.next = current.next.next;
+        }
+        else{
+            System.out.println("Node with data " + data + "could not be found.");
+    }
+    }
+
+public void deleteAfter(int afterData){
+    Node current = head;
+    while(current != null && current.data != afterData){
+        current = current.next;
+    }
+    if (current != null && current.next != null) {
+        current.next = current.next.next; 
+    } else if (current == null) {
+        System.out.println("Node with data " + afterData + " could not be found.");
+    } else {
+        System.out.println("Node with data " + afterData + " has no next node to delete.");
+    }
+}
+}
+
